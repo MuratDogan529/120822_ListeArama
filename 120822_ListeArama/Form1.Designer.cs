c1c7711e6453b266c71f9f59f3f46cb6de75c482
murat.dogan2 <murat.dogan2@ERKHBN0096>
2023-01-04 16:26:43 +0300

    Add project files.

---
+++ 120822_ListeArama/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace _120822_ListeArama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIsim = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIsimAra = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.btnKisiSayisi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İsim:";
+            // 
+            // txtIsim
+            // 
+            this.txtIsim.Location = new System.Drawing.Point(67, 14);
+            this.txtIsim.Name = "txtIsim";
+            this.txtIsim.Size = new System.Drawing.Size(204, 22);
+            this.txtIsim.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(22, 42);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(249, 276);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 330);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "İsim:";
+            // 
+            // txtIsimAra
+            // 
+            this.txtIsimAra.Location = new System.Drawing.Point(74, 324);
+            this.txtIsimAra.Name = "txtIsimAra";
+            this.txtIsimAra.Size = new System.Drawing.Size(197, 22);
+            this.txtIsimAra.TabIndex = 4;
+            this.txtIsimAra.TextChanged += new System.EventHandler(this.txtIsimAra_TextChanged);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(292, 14);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(83, 32);
+            this.btnEkle.TabIndex = 5;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(298, 319);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(83, 32);
+            this.btnAra.TabIndex = 5;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // btnKisiSayisi
+            // 
+            this.btnKisiSayisi.Location = new System.Drawing.Point(292, 52);
+            this.btnKisiSayisi.Name = "btnKisiSayisi";
+            this.btnKisiSayisi.Size = new System.Drawing.Size(83, 32);
+            this.btnKisiSayisi.TabIndex = 5;
+            this.btnKisiSayisi.Text = "Kişi Sayısı";
+            this.btnKisiSayisi.UseVisualStyleBackColor = true;
+            this.btnKisiSayisi.Click += new System.EventHandler(this.btnKisiSayisi_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnEkle;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 375);
+            this.Controls.Add(this.btnKisiSayisi);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtIsimAra);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtIsim);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIsim;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIsimAra;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.Button btnKisiSayisi;
+    }
+}
+
---
+++ 120822_ListeArama/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _120822_ListeArama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if(txtIsim.Text != "")
+            {
+                listBox1.Items.Add(txtIsim.Text.ToUpper().Trim());///ToUpper büyük harfe çevirir//tolower küçük harfe çevi
+                txtIsim.Clear();
+                txtIsim.Focus();
+
+            }
+                
+
+
+        }
+
+        private void btnKisiSayisi_Click(object sender, EventArgs e)
+        {
+            int adet = listBox1.Items.Count;//count elemanları sayar.
+            MessageBox.Show(adet.ToString());
+
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            if(txtIsimAra.Text !=String.Empty)
+            {
+                string isim = txtIsimAra.Text.ToUpper().Trim();
+                if (listBox1.Items.Contains(isim)== true)
+                {
+                    MessageBox.Show("Aranan İsim Listede Var");
+
+                }
+                else
+                {
+                    MessageBox.Show("Aranan İsim Listede Yok");
+                }
+            }
+        }
+
+        private void txtIsimAra_TextChanged(object sender, EventArgs e)
+        {
+            string aranan = txtIsimAra.Text.ToUpper().Trim();
+            ArrayList isimler=new ArrayList();
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                isimler.Add(listBox1.Items[i].ToString());
+            }
+
+            foreach (string item in isimler)
+            {
+                if (!item.Contains(aranan))
+                {
+                    listBox1.Items.Remove(item);
+                }
+
+            }
+            
+        }
+    }
+}
